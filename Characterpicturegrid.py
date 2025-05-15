37cf081a928fb533e90cd0fceff0aafe7b003b78
sanio26 <sanioj.24.beai@acharya.ac.in>
2025-05-15 10:30:59 +0530

    Create Characterpicturegrid.py

---
+++ Characterpicturegrid.py
@@ -0,0 +1,18 @@
+print("Sanio Frederic,1AY24AI099,SEC-O")
+grid = [
+    ['.', '.', '.', '.', '.', '.'],
+    ['.', 'O', 'O', '.', '.', '.'],
+    ['O', 'O', 'O', 'O', '.', '.'],
+    ['O', 'O', 'O', 'O', 'O', '.'],
+    ['.', 'O', 'O', 'O', 'O', 'O'],
+    ['O', 'O', 'O', 'O', 'O', '.'],
+    ['O', 'O', 'O', 'O', '.', '.'],
+    ['.', 'O', 'O', '.', '.', '.'],
+    ['.', '.', '.', '.', '.', '.']
+]
+
+# Rotate and print the grid
+for x in range(len(grid[0])):
+    for y in range(len(grid)):
+        print(grid[y][x], end='')
+    print()
